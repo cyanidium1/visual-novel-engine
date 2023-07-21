d0311ab625ec7a6c6d4a09b06138984df0084412
Fedir <99473719+cyanidium1@users.noreply.github.com>
2023-07-22 01:25:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/visual-novel-engine">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
