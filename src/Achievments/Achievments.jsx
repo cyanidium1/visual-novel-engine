95d2ec24f795876b4d2ca8ad96f3dc1df70c5f0f
Fedir <99473719+cyanidium1@users.noreply.github.com>
2023-07-22 02:28:43 +0200

    upd achievments and credits

--- src/Achievments/Achievments.jsx
+++ src/Achievments/Achievments.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import styles from './Achievments.module.css'
-import { Link } from 'react-router-dom';
+import React from "react";
+import styles from "./Achievments.module.css";
+import { Link } from "react-router-dom";
 
 const Achievments = () => {
-    return (
-        <div className={styles.box}>
-            <Link className={styles.back} to={'/'}>Back</Link>
-            <p>
-                List of achievments should be here
-            </p>
-        </div>
-    );
+  return (
+    <div className={styles.box}>
+      <Link className={styles.back} to={"/"}>
+        Back
+      </Link>
+      <p className={styles.item}>List of achievments should be here</p>
+    </div>
+  );
 };
 
 export default Achievments;
